ea74c520d28239cc096e4ba936b78ba6f1fb5fbd
chuck francis <chuck_francis10@yahoo.com>
2020-08-24 11:53:25 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace finding_indexes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "I like cheese";
+
+            int i = 0; // start index
+
+            // loop until we explicitly break
+            while (true)
+            {
+                // check for an e startingf at index i
+                i = s.IndexOf("e", i);
+
+                if (i == -1)
+                    break; // no more e's
+                else
+                    Console.WriteLine($"Found an e at index: {i}");
+
+                // move to the next index after the e we found.
+                i++;
+
+            }
+        }
+    }
+}
